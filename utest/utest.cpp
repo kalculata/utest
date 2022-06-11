db7ed5eac9d3f95b350817e0b35e054ffa62e5f1
kalculata <nprincehuzaifa@gmail.com>
2022-06-11 20:22:37 +0200

    add test counter

--- utest/utest.cpp
+++ utest/utest.cpp
@@ -3,34 +3,33 @@
 
 void UTEST::ASSERT_EQUAL(int a, int b, std::string message){
 	this->counter++;
+	std::cout << "--------------------------------------- TEST " << this->counter << " ---------------------------------------\n";
 
 	if (a != b) {
-		std::cout << "---------------------------------------\n";
 		std::cout << "[FAILED]: " << a << " != " << b << "\n";
-		std::cout << "---------------------------------------\n";
 		this->failed_test++;
 	}
 	else {
 		std::cout << "[PASS]: " << a << " == " << b << "\n";
 		this->passed_test++;
 	}
+	std::cout << "--------------------------------------------------------------------------------------\n";
 }
 
 void UTEST::ASSERT_EQUAL(int& a, int& b, std::string message){
 	this->counter++;
+	std::cout << "--------------------------------------- TEST " << this->counter << " ---------------------------------------\n";
 
 	if (a != b) {
-		std::cout << "---------------------------------------\n";
 		std::cout << "[FAILED]: " << a << " != " << b << "\n";
-		std::cout << "---------------------------------------\n";
 		this->failed_test++;
 	}
+
 	else {
-		std::cout << "---------------------------------------\n";
 		std::cout << "[PASS]: " << a << " == " << b << "\n";
-		std::cout << "---------------------------------------\n";
 		this->passed_test++;
 	}
+	std::cout << "-----------------------------------------------------------------------------\n";
 }
 
 void UTEST::report() {
